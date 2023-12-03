83140e0b970d727846ce65953bf30ee45ce3af75
YaraGamal5 <yaraagamaal55@gmail.com>
2023-12-03 14:23:08 +0200

    first commit

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name:'cars',
+  initialState: {
+    searchTrem:'',
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+        state.searchTrem = action.payload;
+    },
+    addCar(state, action){
+        state.data.push({
+            name: action.payload.name,
+            cost: action.payload.cost,
+            id: nanoid()
+        });
+    },
+    removeCar(state, action){
+        const updated = state.data.filter((car) => {
+            return car.id !== action.payload;
+        });
+        state.data = updated;
+    },
+  },
+});
+
+
+export const {changeSearchTerm, addCar, removeCar} = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
